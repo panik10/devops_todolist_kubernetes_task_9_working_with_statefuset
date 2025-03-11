910819fb28ad039b2e74109836978296aea19a2c
Pavlo Nikolaiev <workhardpolly@gmail.com>
2025-03-11 15:25:16 +0200

    solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,39 @@
 #!/bin/bash
+
+echo "Creating namespace"
+
+kubectl apply -f .infrastructure/mysql/st-namespace.yml
+
+echo "Starting dns service"
+
+kubectl apply -f .infrastructure/mysql/st-service.yml 
+
+echo "Applying secret"
+
+kubectl apply -f .infrastructure/mysql/st-secret.yml 
+
+echo "Applying config map"
+
+kubectl apply -f .infrastructure/mysql/st-configMap.yml
+
+sleep 5
+
+echo "Starting Stateful Set"
+
+kubectl apply -f .infrastructure/mysql/statefulSet.yml
+
+kubectl apply -f .infrastructure/configMap.yml
+
+kubectl apply -f .infrastructure/secret.yml
+
+kubectl apply -f .infrastructure/pv.yml
+
+kubectl apply -f .infrastructure/pvc.yml
+
+kubectl apply -f .infrastructure/nodeport.yml
+
+kubectl apply -f .infrastructure/clusterIp.yml
+
+kubectl apply -f .infrastructure/hpa.yml
+
+kubectl apply -f .infrastructure/deployment.yml
